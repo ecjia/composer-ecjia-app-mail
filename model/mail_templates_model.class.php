ab0c850886d2bc81a6d2432ea165224cb66ff767
王正东 <wangzhengdong@ecmoban.com>
2020-08-07 09:53:07 +0800

    优化数据模型，指定数据连接池

--- model/mail_templates_model.class.php
+++ model/mail_templates_model.class.php
@@ -46,43 +46,51 @@
 //
 defined('IN_ECJIA') or exit('No permission resources.');
 
-class mail_templates_model extends Component_Model_Model {
-	public $table_name = '';
-	public function __construct() {
-		$this->table_name	= 'mail_templates';
-		parent::__construct();
-	}
+class mail_templates_model extends Component_Model_Model
+{
+    public $table_name = '';
 
-	/**
-	 * 加载指定的模板内容
-	 *
-	 * @access  public
-	 * @param   string  $temp   邮件模板的ID
-	 * @return  array
-	 */
-	public function load_template($tpl) {
-		return RC_DB::table('mail_templates')
-			->where('template_code', $tpl)
-			->select('template_subject', 'template_content', 'is_html')
-			->first();
-	}
-	
-	public function mail_templates_find($id) {
-		return RC_DB::table('mail_templates')->where('template_id', $id)->first();
-	}
-	
-	public function mail_templates_select($where, $field='*') {
-		return RC_DB::table('mail_templates')
-			->where('type', $where)
-			->select($field)
-			->get();
-	}
-	
-	public function mail_templates_update($where, $data) {
-		return RC_DB::table('mail_templates')
-			->where('template_code', $where)
-			->update($data);
-	}
+    public function __construct()
+    {
+        $this->db_setting = config('cashier.database_connection', 'default');
+        $this->table_name = 'mail_templates';
+        parent::__construct();
+    }
+
+    /**
+     * 加载指定的模板内容
+     *
+     * @access  public
+     * @param string $temp 邮件模板的ID
+     * @return  array
+     */
+    public function load_template($tpl)
+    {
+        return RC_DB::table('mail_templates')
+            ->where('template_code', $tpl)
+            ->select('template_subject', 'template_content', 'is_html')
+            ->first();
+    }
+
+    public function mail_templates_find($id)
+    {
+        return RC_DB::table('mail_templates')->where('template_id', $id)->first();
+    }
+
+    public function mail_templates_select($where, $field = '*')
+    {
+        return RC_DB::table('mail_templates')
+            ->where('type', $where)
+            ->select($field)
+            ->get();
+    }
+
+    public function mail_templates_update($where, $data)
+    {
+        return RC_DB::table('mail_templates')
+            ->where('template_code', $where)
+            ->update($data);
+    }
 }
 
 // end
