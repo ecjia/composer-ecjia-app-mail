b0dc386be2090efcbf1015ff29f02029e4e449f0
王正东 <wangzhengdong@ecmoban.com>
2020-10-19 11:13:33 +0800

    更新邮件菜单定义

--- classes/Services/AdminMenuService.php
+++ classes/Services/AdminMenuService.php
@@ -25,6 +25,7 @@ class AdminMenuService
             ecjia_admin::make_admin_menu('divider', '', '', 10)->add_purview('mail_template_manage'),
             ecjia_admin::make_admin_menu('mail_template', __('邮件模板', 'mail'), RC_Uri::url('mail/admin_template/init'), 11)->add_purview('mail_template_manage'),
             ecjia_admin::make_admin_menu('mail_event', __('邮件事件', 'mail'), RC_Uri::url('mail/admin_events/init'), 13)->add_purview('mail_events_manage'),
+            ecjia_admin::make_admin_menu('mail_plugin', __('邮件渠道', 'mail'), RC_Uri::url('mail/admin_plugin/init'), 14)->add_purview('mail_plugin_manage'),
         );
 
         $menus->add_submenu($submenus);
