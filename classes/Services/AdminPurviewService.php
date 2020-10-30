c192d60e6714d7c99685ee65ada05872e843ed33
周瑞丽 <zhouruili@ecmoban.com>
2020-10-30 16:06:39 +0800

    工单#2546平台后台权限部分删减修改

--- classes/Services/AdminPurviewService.php
+++ classes/Services/AdminPurviewService.php
@@ -16,13 +16,13 @@ class AdminPurviewService
     public function handle(& $options)
     {
         $purviews = array(
-            array('action_name' => __('邮件订阅管理', 'mail'), 	'action_code' => 'email_list_manage', 'relevance' => ''),
-            array('action_name' => __('邮件订阅更新', 'mail'), 	'action_code' => 'email_list_update', 'relevance' => ''),
-            array('action_name' => __('邮件订阅删除', 'mail'),	'action_code' => 'email_list_delete', 'relevance' => ''),
+//             array('action_name' => __('邮件订阅管理', 'mail'), 	'action_code' => 'email_list_manage', 'relevance' => ''),
+//             array('action_name' => __('邮件订阅更新', 'mail'), 	'action_code' => 'email_list_update', 'relevance' => ''),
+//             array('action_name' => __('邮件订阅删除', 'mail'),	'action_code' => 'email_list_delete', 'relevance' => ''),
 
-            array('action_name' => __('邮件队列管理', 'mail'), 	'action_code' => 'email_sendlist_manage',	'relevance' => ''),
-            array('action_name' => __('邮件队列发送', 'mail'), 	'action_code' => 'email_sendlist_send', 	'relevance' => ''),
-            array('action_name' => __('邮件队列删除', 'mail'), 	'action_code' => 'email_sendlist_delete',	'relevance' => ''),
+//             array('action_name' => __('邮件队列管理', 'mail'), 	'action_code' => 'email_sendlist_manage',	'relevance' => ''),
+//             array('action_name' => __('邮件队列发送', 'mail'), 	'action_code' => 'email_sendlist_send', 	'relevance' => ''),
+//             array('action_name' => __('邮件队列删除', 'mail'), 	'action_code' => 'email_sendlist_delete',	'relevance' => ''),
 
             array('action_name' => __('邮件模板管理', 'mail'),    'action_code' => 'mail_template_manage', 'relevance' => ''),
             array('action_name' => __('邮件模板更新', 'mail'),    'action_code' => 'mail_template_update', 'relevance' => ''),
--- configs/package.php
+++ configs/package.php
@@ -52,7 +52,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
 return array(
     'identifier'  => 'ecjia.mail',
     'directory'   => 'mail',
-    'name'        => __('邮件', 'mail'),
+    'name'        => __('邮件管理', 'mail'),
     'description' => __('邮件应用以“电子邮件为主要的网站推广手段”为核心功能打造开发，针对商城用户对商品订阅、关注来接收最新邮件动态，而商家则可以对促销，打折，红包、杂志等促销信息对全体、指定（等级）用户进行邮件群发推送，并且可以实时关注邮件发送队列的进度。', 'mail'),
     'author'      => 'ECJIA TEAM',
     'website'     => 'http://www.ecjia.com',
