2cbbb0b8a687e5976d187aed4959efdc1bd6c3fd
王正东 <wangzhengdong@ecmoban.com>
2020-12-21 16:08:28 +0800

    废弃邮件订阅管理菜单

--- classes/Services/AdminMenuService.php
+++ classes/Services/AdminMenuService.php
@@ -25,8 +25,6 @@ class AdminMenuService
             ecjia_admin::make_admin_menu('mail_plugin', __('邮件渠道', 'mail'), RC_Uri::url('mail/admin_plugin/init'), 14)->add_purview('mail_plugin_manage'),
             ecjia_admin::make_admin_menu('mail_test', __('发送测试邮件', 'mail'), RC_Uri::url('mail/admin_mail_test/init'), 14)->add_purview('mail_plugin_manage'),
             ecjia_admin::make_admin_menu('view_sendlist', __('邮件发送队列', 'mail'), RC_Uri::url('mail/admin_view_sendlist/init'), 16)->add_purview('email_sendlist_manage'),
-            ecjia_admin::make_admin_menu('divider', '', '', 20)->add_purview('mail_template_manage'),
-            ecjia_admin::make_admin_menu('email_list', __('邮件订阅管理', 'mail'), RC_Uri::url('mail/admin_email_list/init'), 21)->add_purview('email_list_manage'),
         );
 
         $menus->add_submenu($submenus);
