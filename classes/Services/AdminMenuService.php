22e5ace384f30ec089978d2a565710cdfc88899a
王正东 <wangzhengdong@ecmoban.com>
2020-12-21 16:10:31 +0800

    修改邮件菜单

--- classes/Services/AdminMenuService.php
+++ classes/Services/AdminMenuService.php
@@ -24,7 +24,7 @@ class AdminMenuService
             ecjia_admin::make_admin_menu('mail_event', __('邮件事件', 'mail'), RC_Uri::url('mail/admin_events/init'), 13)->add_purview('mail_events_manage'),
             ecjia_admin::make_admin_menu('mail_plugin', __('邮件渠道', 'mail'), RC_Uri::url('mail/admin_plugin/init'), 14)->add_purview('mail_plugin_manage'),
             ecjia_admin::make_admin_menu('mail_test', __('发送测试邮件', 'mail'), RC_Uri::url('mail/admin_mail_test/init'), 14)->add_purview('mail_plugin_manage'),
-            ecjia_admin::make_admin_menu('view_sendlist', __('邮件发送队列', 'mail'), RC_Uri::url('mail/admin_view_sendlist/init'), 16)->add_purview('email_sendlist_manage'),
+            ecjia_admin::make_admin_menu('view_sendlist', __('邮件发送队列', 'mail'), RC_Uri::url('mail/admin_mail_sendlist/init'), 16)->add_purview('email_sendlist_manage'),
         );
 
         $menus->add_submenu($submenus);
